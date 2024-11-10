005ffa4182b8b440ea072a1880fd7f5c728c7e3d
MargheritaMottana <margherita.mottana@gmail.com>
2024-11-10 19:16:06 +0100

    creato componente ProjectCard,  importato in AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,67 +1,19 @@
 <script>
-
 // importato axios per chiamata api
 import axios from 'axios';
+
+// import componenti
+import ProjectCard from './partials/ProjectCard.vue';
 
 export default{
   data(){
     return{
-      projects:[],
-      prevPage: null,
-      nextPage: null,
-      clickedButton: false,
     };
   },
-  // per far partire la chiamata la devo richiamare
-  mounted(){
-    this.getProjects();
+  components:{
+    ProjectCard,
   },
   methods:{
-    // chiamata api
-    getProjects(){
-      axios
-        .get('http://127.0.0.1:8000/api/projects')
-        .then((res)=>{
-          this.projects = res.data.projects.data;
-          console.log(this.projects);
-
-          // bottoni
-          this.prevPage = res.data.projects.prev_page_url;
-          this.nextPage = res.data.projects.next_page_url;
-        });
-    },
-    // bottone prev
-    toPrevPage(){
-      this.clickedButton = true;
-      axios
-        .get(this.prevPage)
-        .then((res)=>{
-          this.projects = res.data.projects.data;
-          console.log(this.projects);
-
-          // bottoni
-          this.prevPage = res.data.projects.prev_page_url;
-          this.nextPage = res.data.projects.next_page_url;
-
-          this.clickedButton = false;
-        });
-    },
-    // bottone next
-    toNextPage(){
-      this.clickedButton = true;
-      axios
-        .get(this.nextPage)
-        .then((res)=>{
-          this.projects = res.data.projects.data;
-          console.log(this.projects);
-
-          // bottoni
-          this.prevPage = res.data.projects.prev_page_url;
-          this.nextPage = res.data.projects.next_page_url;
-
-          this.clickedButton = false;
-        });
-    },
   },
 }
 
@@ -71,62 +23,7 @@ export default{
 
   <main>
 
-    <div class="container py-5">
-
-      <div class="row">
-
-        <div class="col" v-for="project in projects" :key="project.id">
-
-          <div class="card mb-4">
-
-            <img :src="project.cover" class="card-img-top" :alt="project.title">
-
-            <div class="card-body">
-                <h5 class="card-title">
-                  {{project.id}}) {{project.title}}
-                </h5>
-
-                <p class="card-text">{{project.description}}</p>
-
-                <div class="mb-3" v-if="project.type !=null">
-                  {{ project.type.title }}
-                </div>
-                <div class="mb-3" v-else>
-                  Without Type
-                </div>
-
-                <span v-for="technology in project.technologies" :key="technology.id" class="badge rounded-pill text-bg-primary me-2">
-                  {{ technology.title }}
-                </span>
-            </div>
-
-          </div>
-
-        </div>
-
-        <div class="d-flex justify-content-center">
-
-          <button 
-            @click="toPrevPage()"
-            :disabled="prevPage == null || clickedButton" 
-            type="button" 
-            class="btn btn-primary m-2">
-            ⁠☜(⸝⸝•ᴗ•⸝⸝ ⁠☜) Prev
-          </button>
-
-          <button
-            @click="toNextPage()"
-            :disabled="nextPage == null || clickedButton" 
-            type="button"
-            class="btn btn-primary m-2">
-            Next (☞ ⸝⸝•ᴗ•⸝⸝)⁠☞
-          </button>
-
-        </div>
-
-      </div>
-
-    </div>
+    <ProjectCard />
 
   </main>
 
